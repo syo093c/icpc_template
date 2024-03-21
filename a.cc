a56d148410e9ab05a46be9d90ecbe5a657910765
syo <syo093c@gmail.com>
2024-03-21 16:43:31 +0900

    upddate

--- a.cc
+++ a.cc
@@ -1,35 +1,32 @@
 #ifndef LOCAL
-#pragma GCC optimize ("Ofast")
-#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
 #endif
 
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll=long long;
-#define rng(i,a,b) for(int i=int(a);i<int(b);i++)
-#define rep(i,b) rng(i,0,b)
+using ll = long long;
+#define rng(i, a, b) for (int i = int(a); i < int(b); i++)
+#define rep(i, b) rng(i, 0, b)
 #define pb push_back
 #define eb emplace_back
-#define all(x) x.begin(),x.end()
+#define all(x) x.begin(), x.end()
 #define si(x) int(x.size())
-#define tct template<class t>
-tct using vc=vector<t>;
+#define tct template <class t>
+tct using vc = vector<t>;
 
-using P=pair<int,int>;
+using P = pair<int, int>;
 #define a first
 #define b second
 #define mp make_pair
 using uint = unsigned int;
-const uint mod=998244353;
-using ull unsigned ll;
+const uint mod = 998244353;
 
-void solve(){
+void solve() {}
 
-}
-
-int main(){
-	solve();
+int main() {
+  solve();
   return 0;
 }
 
